daaeadfc13e157de081e5e1bcfc95e476e86450b
MahmoudAbdulfattah1 <93574572+MahmoudAbdulfattah1@users.noreply.github.com>
2021-11-02 17:37:38 +0200

    Update client.java

--- client.java
+++ client.java
@@ -1,13 +1,37 @@
 package com.company;
+
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class client {
+    static Scanner inp = new Scanner(System.in);
+
     public static void main(String[] args) throws IOException {
-        Socket s=new Socket("localhost",4999);
-        InputStreamReader ip=new InputStreamReader(s.getInputStream());
-        BufferedReader bf=new BufferedReader(ip);
-        System.out.println(bf.readLine());
+        DataOutputStream out = null;
+        DataInputStream in = null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        try {
+            Socket s = new Socket("localhost", 9999);
+            inputStreamReader = new InputStreamReader(s.getInputStream());
+            outputStreamWriter = new OutputStreamWriter((s.getOutputStream()));
+            bufferedReader = new BufferedReader(inputStreamReader);
+            bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+            while (true) {
+                String str = inp.nextLine();
+                bufferedWriter.write(str);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
 
     }
 }
