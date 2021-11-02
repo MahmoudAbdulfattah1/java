d01adca417f7f25ba63f5d2e02f5fa2a112f31cf
MahmoudAbdulfattah1 <93574572+MahmoudAbdulfattah1@users.noreply.github.com>
2021-11-02 17:38:04 +0200

    Update server.java

--- server.java
+++ server.java
@@ -1,19 +1,34 @@
 package com.company;
+
 import java.net.*;
 import java.io.*;
 
 public class server {
     public static void main(String[] args) throws IOException {
-        ServerSocket ss=new ServerSocket(4999);
-        while(true) {
-            Socket s = ss.accept();
-            System.out.println("client connected");
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(ss.accept().getOutputStream());
-            BufferedWriter bw = new BufferedWriter(outputStreamWriter);
-            bw.write("Ping");
-            bw.newLine();
-            bw.flush();
+        DataOutputStream out = null;
+        DataInputStream in = null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        Socket socket = null;
+        ServerSocket ss = new ServerSocket(9999);
+        while (true) {
+            socket = ss.accept();
+            inputStreamReader = new InputStreamReader(socket.getInputStream());
+            bufferedReader = new BufferedReader(inputStreamReader);
+            System.out.println("Client connected");
+            try {
+                while (true) {
+                    String str = bufferedReader.readLine();
+                    System.out.println("client:" + str);
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+
 
     }
 }
